ad734fcf2078137f4284640f7b8674d5d3d9da62
zhrgithub <zhr_java@163.com>
2022-03-01 11:26:08 +0800

    整理aio的代码，添加对应的注释，并分析性能消耗

--- JdkLearn/src/main/java/com/learnjava/io/aio/ReadFromFile.java
+++ JdkLearn/src/main/java/com/learnjava/io/aio/ReadFromFile.java
@@ -2,63 +2,35 @@ package com.learnjava.io.aio;
 
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
-import java.nio.channels.CompletionHandler;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.concurrent.Future;
 
 public class ReadFromFile {
     public static void main(String[] args) throws Exception {
-        Path file = Paths.get("/usr/a.txt");
+        Path file = Paths.get("/Users/zhr/Downloads/a.txt");
         AsynchronousFileChannel channel = AsynchronousFileChannel.open(file);
 
         ByteBuffer buffer = ByteBuffer.allocate(100_000);
+        //读取管道中的内容
         Future<Integer> result = channel.read(buffer, 0);
 
+        //判断是否读取完成，没有完成的话一直是读取中，对CPU内存消耗很大
         while (!result.isDone()) {
             ProfitCalculator.calculateTax();
         }
+        //文本总共被读取了多少个字节
         Integer bytesRead = result.get();
         System.out.println("Bytes read [" + bytesRead + "]");
     }
 }
 class ProfitCalculator {
     public ProfitCalculator() {
+        System.out.println("Profit Calculator completed !!!");
     }
     public static void calculateTax() {
+        System.out.println("计算税务完毕");
     }
 }
 
- class WriteToFile {
-
-    public static void main(String[] args) throws Exception {
-        AsynchronousFileChannel fileChannel = AsynchronousFileChannel.open(
-                Paths.get("/asynchronous.txt"), StandardOpenOption.READ,
-                StandardOpenOption.WRITE, StandardOpenOption.CREATE);
-        CompletionHandler<Integer, Object> handler = new CompletionHandler<Integer, Object>() {
-
-            @Override
-            public void completed(Integer result, Object attachment) {
-                System.out.println("Attachment: " + attachment + " " + result
-                        + " bytes written");
-                System.out.println("CompletionHandler Thread ID: "
-                        + Thread.currentThread().getId());
-            }
-
-            @Override
-            public void failed(Throwable e, Object attachment) {
-                System.err.println("Attachment: " + attachment + " failed with:");
-                e.printStackTrace();
-            }
-        };
-
-        System.out.println("Main Thread ID: " + Thread.currentThread().getId());
-        fileChannel.write(ByteBuffer.wrap("Sample".getBytes()), 0, "First Write",
-                handler);
-        fileChannel.write(ByteBuffer.wrap("Box".getBytes()), 0, "Second Write",
-                handler);
-
-    }
-}
 
