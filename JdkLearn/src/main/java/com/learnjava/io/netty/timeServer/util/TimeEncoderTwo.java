7e6629b496eeea51f44d85ef3e93c971d3ac9440
zhrgithub <zhr_java@163.com>
2022-03-08 15:34:26 +0800

    修复编码解码时间服务器问题

---
+++ JdkLearn/src/main/java/com/learnjava/io/netty/timeServer/util/TimeEncoderTwo.java
@@ -0,0 +1,24 @@
+package com.learnjava.io.netty.timeServer.util;
+
+import com.learnjava.io.netty.timeServer.pojo.UnixTime;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+
+/**
+ * @author zhr_java@163.com
+ * @date 2022/3/8 15:33
+ */
+public class TimeEncoderTwo extends ChannelOutboundHandlerAdapter {
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+
+        UnixTime m = (UnixTime) msg;
+        ByteBuf encoded = ctx.alloc().buffer(4);
+        encoded.writeInt((int)m.value());
+        ctx.write(encoded, promise); // (1)
+    }
+
+}
